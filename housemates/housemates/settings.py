55cf798e88d2de03404f2347dd0391df06b53022
Serhii Zolotov <freelancetripper@gmail.com>
2024-01-14 12:28:48 -0500

    - Created react app - installed all Dependencies

--- housemates/housemates/settings.py
+++ housemates/housemates/settings.py
@@ -47,6 +47,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
